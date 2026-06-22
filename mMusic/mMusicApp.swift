7873a7af5bb1fbb95d614b9e2c69a61f4a6d82d7
vanya <mitrofanovvana20082@gmail.com>
2026-06-22 10:23:29 +0200

    Initial Commit

---
+++ mMusic/mMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  mMusicApp.swift
+//  mMusic
+//
+//  Created by Vanya on 22.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct mMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
