c56c9a05da801b0fadd389b6751070f65b1fbe88
vanya <mitrofanovvana20082@gmail.com>
2026-06-22 15:05:05 +0200

    feat: add music uploader

--- mMusic/Features/Download/Views/DownloadView.swift
+++ mMusic/Features/Download/Views/DownloadView.swift
@@ -6,10 +6,54 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct DownloadView: View {
+    @State private var isImporting = false
+    @State private var isLoading = false
+    
     var body: some View {
-        Text("Download")
-            .navigationTitle("Download")
+        VStack {
+            HStack(spacing: 16) {
+                Button {
+                    isImporting = true
+                } label: {
+                    VStack {
+                        Text("Upload music")
+                        Image(systemName: "square.and.arrow.down")
+                    }
+                    .frame(maxWidth: .infinity, minHeight: 120)
+                }
+                .buttonStyle(.glassProminent)
+                .buttonBorderShape(.roundedRectangle(radius: 15))
+                .tint(.orange)
+
+                Button {
+
+                } label: {
+                    VStack {
+                        Text("From YouTube")
+                        Image(systemName: "video")
+                    }
+                    .frame(maxWidth: .infinity, minHeight: 120)
+                }
+                .buttonStyle(.glassProminent)
+                .buttonBorderShape(.roundedRectangle(radius: 15))
+                .tint(.red)
+            }
+            .fileImporter(isPresented: $isImporting, allowedContentTypes: [.audio], onCompletion: handleFileLoading)
+            .padding()
+
+            Spacer()
+            
+            if isLoading {
+                Text("File is loading...")
+            }
+        }
+        .navigationTitle("Download")
     }
+}
+
+private func handleFileLoading(_ result: Result<URL, Error>) {
+    print(result)
 }
