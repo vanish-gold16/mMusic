156b812b0922db70706413be248ed50f3721b14c
vanya <mitrofanovvana20082@gmail.com>
2026-06-27 19:15:40 +0200

    feat: add searchable

--- mMusic/Features/Search/Views/SearchView.swift
+++ mMusic/Features/Search/Views/SearchView.swift
@@ -1,8 +1,11 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText: String = ""
+    
     var body: some View {
         Text("Search")
             .navigationTitle("Search")
+            .searchable(text: $searchText)
     }
 }
