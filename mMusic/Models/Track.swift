7a083df1049219e0b06a63b200073ece0b2f70aa
vanya <mitrofanovvana20082@gmail.com>
2026-06-30 23:56:48 +0200

    feat: change Track to @Model

--- mMusic/Models/Track.swift
+++ mMusic/Models/Track.swift
@@ -1,10 +1,17 @@
 import Foundation
+import SwiftData
 
-struct Track: Identifiable, Codable {
-    let id = UUID()
+@Model
+class Track {
     var name: String
     var artist: String
     let filename: String
-    
     var isFavourite = false
+    
+    init(name: String, artist: String, filename: String, isFavourite: Bool = false) {
+        self.name = name
+        self.artist = artist
+        self.filename = filename
+        self.isFavourite = isFavourite
+    }
 }
